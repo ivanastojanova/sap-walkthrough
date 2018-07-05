809ef2d92af30296b3f10921e43afa5ed948dcd4
ivanastojanova <ivana.kareva@gmail.com>
2018-07-05 09:47:41 +0000

    i18n model step 8

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,56 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel"
-], function (Controller,MessageToast, JSONModel) {
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (Controller,MessageToast, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("Walkthrough.controller.App", {
    	  onInit: function () {
-         var oData = {
+         
+         var JsonData = {
             recipient : {
-               name : "World"
+               name : "World",
+               lastname: "Beautyful"
             }
          };
-         var oModel = new JSONModel(oData);
-         this.getView().setModel(oModel);
+         
+         var jModel = new JSONModel(JsonData);
+         
+         
+         this.getView().setModel(jModel);
+         
+         var i18nModel = new ResourceModel({
+            bundleName: "Walkthrough.i18n.i18n"
+         });
+         
+         this.getView().setModel(i18nModel, "i18n"); 
    	  },
       onShowHello : function () {
          // show a native JavaScript alert
-         MessageToast.show("Hello World");
-         MessageToast.show('What about this!!!');
+         /*MessageToast.show("Hello World");
+         MessageToast.show('What about this!!!');*/
+         
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         
+         var sMsg = oBundle.getText("helloMsg", [sRecipient,'Ivana']);
+         
+         MessageToast.show(sMsg,{
+			    duration: 3000,                  // default
+			    width: "15em",                   // default
+			    my: "center center",             // default
+			    at: "center center",             // default
+			    of: window,                      // default
+			    offset: "0 0",                   // default
+			    collision: "fit fit",            // default
+			    onClose: null,                   // default
+			    autoClose: true,                 // default
+			    animationTimingFunction: "ease", // default
+			    animationDuration: 1000,         // default
+			    closeOnBrowserNavigation: true   // default
+			});
       }
    });
 });
