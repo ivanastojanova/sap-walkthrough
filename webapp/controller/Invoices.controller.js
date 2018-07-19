fe684aa0b5c5ac386db06a155719ac542b269532
ivanastojanova <ivana.kareva@gmail.com>
2018-07-19 10:56:14 +0000

    Expression Bindings and Custom Formatters Step 22 and 23

--- webapp/controller/Invoices.controller.js
+++ webapp/controller/Invoices.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function(Controller,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"Walkthrough/model/formatter"
+
+], function(Controller,JSONModel,formatter) {
 	"use strict";
 
 	return Controller.extend("Walkthrough.controller.Invoices", {
@@ -11,12 +13,13 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf Walkthrough.view.Invoices
 		 */
+		    formatter: formatter,
 			onInit: function() {
 					var oViewModel = new JSONModel({
 						currency: "EUR"
 					});
 					this.getView().setModel(oViewModel, "view_one");
-			},
+			}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
